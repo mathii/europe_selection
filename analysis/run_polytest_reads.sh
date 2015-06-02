b1a57b3dff14c7ce0cfafa78d714a05b2d5672b1
Iain Mathieson <iain_mathieson@hms.harvard.edu>
2015-06-01 20:23:43 -0400

    run polytest

--- analysis/run_polytest_reads.sh
+++ analysis/run_polytest_reads.sh
@@ -1,22 +1,33 @@
-LDIR=~/selection/data/
-DATA=~/selection/counts/all.reads.freq
+V=v6
+LDIR=~/selection/analysis/files/
+DATA=~/selection/counts/${V}/all.reads.freq
 
 while read a b
 do
-#	mkdir -p ~/selection/analysis/poly/${a}
-	OUT=~/selection/analysis/poly/${a}
-#	for what in vsCEU place time
-#	do
-#		python ~/spindrift/Qx.py -q ${DATA} -n 10000 \
-#		-p ${LDIR}/polypairs_${what}.txt \
-#		-o ${OUT}/${a}_${what}_reads -v \
-#		-g ${b} 2> ${OUT}/${a}_${what}_reads.log
-#	done
+    mkdir -p ~/selection/analysis/poly/${a}
+    OUT=~/selection/analysis/poly/${a}
+    for what in vsCEU place time
+    do
+    python ~/spindrift/Qx.py -q ${DATA} -n 10000 \
+    -p ${LDIR}/polypairs_${what}.txt \
+    -o ${OUT}/${a}_${what}_reads -v \
+    -g ${b} 2> ${OUT}/${a}_${what}_reads.log
+    done
 
-	python ~/spindrift/Qx.py -q ${DATA} -n 10000 \
-        -p ${LDIR}/polyall.txt \
-        -o ${OUT}/${a}_reads -v \
-        -g ${b} 2> ${OUT}/${a}_all_reads.log
+    what=vsSteppeMN
+    for extra in Steppe_MN Steppe_MN_no_Srubs
+    do
+    DATA=~/selection/counts/all.reads.${extra}.freq
+    python ~/spindrift/Qx.py -q ${DATA} -n 10000 \
+    -p ${LDIR}/polypairs_${what}.txt \
+    -o ${OUT}/${a}_${what}_${extra}_reads -v \
+    -g ${b} 2> ${OUT}/${a}_${what}_reads.log
+    done
+
+    python ~/spindrift/Qx.py -q ${DATA} -n 10000 \
+    -p ${LDIR}/polyall.txt \
+    -o ${OUT}/${a}_reads -v \
+    -g ${b} 2> ${OUT}/${a}_all_reads.log
 
 done < ~/selection/data/traits.txt
 	
