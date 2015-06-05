a98796fc9142220a536b59faa510c9d06e1d8d3c
Iain Mathieson <iain_mathieson@hms.harvard.edu>
2015-06-04 20:13:25 -0400

    Simple resclaed cov scan

--- analysis/genome_wide_scan_plots.R
+++ analysis/genome_wide_scan_plots.R
@@ -9,14 +9,16 @@ source("~/selection/code/lib/mh_plot_lib.R")
 results.tag <- ""
 version <- "" 
 degf <- 4
+what <- "gsan"
 cA <- commandArgs(TRUE)
 if(length(cA)){
     results.tag <- cA[1]
     version <- cA[2]
     degf <- as.numeric(cA[3])
+    if(length(cA)>3){what <- cA[4]}
 }
 
-results <- paste0("~/selection/analysis/",version,"/gscan/scan_results", results.tag, ".txt")
+results <- paste0("~/selection/analysis/",version,"/", what ,"/scan_results", results.tag, ".txt")
 snpdata <- paste0("~/data/",version,"/use/",version,"1kg_europe2names.snp")
 
 ##############################################################
@@ -35,7 +37,7 @@ if(!("corrected.p"  %in% names(results))){
     lambda <- median(results[neutral&include,"ChiSq"])/qchisq(0.5, df=degf)
     corrected.p <- pchisq(results[,"ChiSq"]/lambda, df=degf, lower.tail=F)
     results <- cbind(results, corrected.p)
-    write.table(results, paste0("~/selection/analysis/",version,"/gscan/scan_results", results.tag, ".txt"),
+    write.table(results, paste0("~/selection/analysis/",version,"/", what ,"/scan_results", results.tag, ".txt"),
                 row.names=FALSE, col.names=TRUE, quote=FALSE, sep="\t")
     cat(paste0("Used lambda = ", lambda, "\n"))
 }else{cat("Using corrected p-values\n")}
@@ -55,7 +57,7 @@ cat(paste0("Used sig.level = ", sig.level, "\n"))
 ## Manhattan plot
 ## png(paste0("~/Dropbox/posters/AAPA/mh_plot", results.tag ,".png"), width=16, height=8, units="in", res=400)
 ## par(bg = "grey92")
-png(paste0("~/selection/analysis/",version,"/gscan/mh_plot", results.tag ,".png"), width=800, height=400)
+png(paste0("~/selection/analysis/",version,"/", what ,"/mh_plot", results.tag ,".png"), width=800, height=400)
 par(mar=c(2,4,1,1))
 MH.plot(res, color.loci=data.frame())
 abline(h=sig.level, col="red", lty=2)
@@ -64,7 +66,7 @@ dev.off()
 ## Per-chromosome Manhattan plots
 sig.chrs <- unique(res$CHR[res$PVAL<(10^-(sig.level))])
 for(c in sig.chrs){
-    png(paste0("~/selection/analysis/",version,"/gscan/mh_plot", results.tag ,".chr", c, ".png"), width=250, height=250)
+    png(paste0("~/selection/analysis/",version,"/", what ,"/mh_plot", results.tag ,".chr", c, ".png"), width=250, height=250)
     ## png(paste0("~/Dropbox/posters/AAPA/mh_plot.chr", c ,".png"), width=4, height=4, units="in", res=400)
     ## par(bg = "grey92")
     par(mar=c(1,2,1,1))
@@ -80,7 +82,7 @@ selection$TAG <- paste(selection[,2], selection[,4], sep=":")
 #First show the effect of genomic control on "neutral" snps.
 neutral <- res[!(res$TAG %in% selection$TAG),]
 selection <- res[(res$TAG %in% selection$TAG),]
-png(paste0("~/selection/analysis/",version,"/gscan/qq_plot_all", results.tag, ".png"), width=400, height=400)
+png(paste0("~/selection/analysis/",version,"/", what ,"/qq_plot_all", results.tag, ".png"), width=400, height=400)
 par(mar=c(4,4,1,1))
 qqPlotOfPValues(neutral$PVAL, col="#377EBA", linecol="black", ci=FALSE, xlim=c(0,6), ylim=c(0,30))
 draw.qq.ci(NROW(neutral), border.col="#377EBA20", fill.col="#377EBA20")
@@ -94,7 +96,7 @@ cat.data$GWAS <- ifelse(cat.data$GWAS|cat.data$Pheno1|cat.data$Pheno2,1,0)
 cat.data$Immune <- cat.data$Pheno3
 cat.data$TAG <- paste(cat.data$CHR, cat.data$POS, sep=":")
 
-png(paste0("~/selection/analysis/",version,"/gscan/qq_plot_cat", results.tag, ".png"), width=400, height=400)
+png(paste0("~/selection/analysis/",version,"/", what ,"/qq_plot_cat", results.tag, ".png"), width=400, height=400)
 par(mar=c(4.1,4.1,1,1))
 cats <- c("GWAS", "CMS", "HiDiff", "Immune", "HLA")
 cols <- brewer.pal(length(cats), "Set1")
