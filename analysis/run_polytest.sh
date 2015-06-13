d6ef8318c1fb028bbad1b69eb4ba547ca697110d
Iain Mathieson <iain_mathieson@hms.harvard.edu>
2015-06-12 21:10:14 -0400

    polymap

--- analysis/run_polytest.sh
+++ analysis/run_polytest.sh
@@ -3,15 +3,15 @@ DATA=~/data/v6/use/v61kg_polynames
 
 while read a b
 do
-	mkdir -p ~/selection/analysis/poly/${a}
-	OUT=~/selection/analysis/poly/${a}
-	for what in vsCEU place time
-	do
-		python ~/spindrift/Qx.py -d ${DATA} -n 10000 \
-		-p ${LDIR}/polypairs_${what}.txt \
-		-o ${OUT}/${a}_${what} -v \
-		-g ${b} -i ${LDIR}/polypops_inbred.txt \ 
-		2> ${OUT}/${a}_${what}.log
-	done
+mkdir -p ~/selection/analysis/poly/${a}
+OUT=~/selection/analysis/poly/${a}
+for what in vsCEU place time
+do
+python ~/spindrift/Qx.py -d ${DATA} -n 10000 \
+-p ${LDIR}/polypairs_${what}.txt \
+-o ${OUT}/${a}_${what} -v \
+-g ${b} -i ${LDIR}/polypops_inbred.txt \ 
+2> ${OUT}/${a}_${what}.log
+done
 done < ~/selection/data/traits.txt
 	
--- analysis/run_polytest_reads.sh
+++ analysis/run_polytest_reads.sh
@@ -1,33 +1,35 @@
+dataset=$1
 V=v6
 LDIR=~/selection/code/files/
-DATA=~/selection/counts/${V}/all.reads.freq
+DATA=~/selection/counts/${V}/all.reads${dataset}.freq
 
 while read a b
 do
-    mkdir -p ~/selection/analysis/${V}/poly/${a}
-    OUT=~/selection/analysis/${V}/poly/${a}
-    for what in vsCEU place time
-    do
-    python ~/spindrift/Qx.py -q ${DATA} -n 10000 \
-    -p ${LDIR}/polypairs_${what}.txt \
-    -o ${OUT}/${a}_${what}_reads -v \
-    -g ${b} 2> ${OUT}/${a}_${what}_reads.log
-    done
+mkdir -p ~/selection/analysis/${V}/poly/${a}
+OUT=~/selection/analysis/${V}/poly/${a}
+for what in pairs
+do
+bsub -q reich "python \
+~/spindrift/Qx.py -q ${DATA} -n 100000 \
+-p ${LDIR}/polypairs_${what}.txt \
+-o ${OUT}/${a}_${what}_reads${dataset} -v \
+-g ${b} 2> ${OUT}/${a}_${what}_reads${dataset}.log"
+done
 
-    what=vsSteppeMN
-    for extra in Steppe_MN Steppe_MN_no_Srubs
-    do
-    DATA=~/selection/counts/${V}/all.reads.${extra}.freq
-    python ~/spindrift/Qx.py -q ${DATA} -n 10000 \
-    -p ${LDIR}/polypairs_${what}.txt \
-    -o ${OUT}/${a}_${what}_${extra}_reads -v \
-    -g ${b} 2> ${OUT}/${a}_${what}_${extra}_reads.log
-    done
+#    what=vsSteppeMN
+#    for extra in Steppe_MN Steppe_MN_no_Srubs
+#    do
+#    DATA=~/selection/counts/${V}/all.reads.${extra}.freq
+#    python ~/spindrift/Qx.py -q ${DATA} -n 10000 \
+#    -p ${LDIR}/polypairs_${what}.txt \
+#    -o ${OUT}/${a}_${what}_${extra}_reads -v \
+#    -g ${b} 2> ${OUT}/${a}_${what}_${extra}_reads.log
+#    done
 
-    python ~/spindrift/Qx.py -q ${DATA} -n 10000 \
-    -p ${LDIR}/polyall.txt \
-    -o ${OUT}/${a}_reads -v \
-    -g ${b} 2> ${OUT}/${a}_all_reads.log
+#    python ~/spindrift/Qx.py -q ${DATA} -n 10000 \
+#    -p ${LDIR}/polyall.txt \
+#    -o ${OUT}/${a}_reads -v \
+#    -g ${b} 2> ${OUT}/${a}_all_reads.log
 
 done < ~/selection/data/traits.txt
 	
